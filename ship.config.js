3aa310246b066bf377cab8d84b202592a9afb7b2
kazuya kawaguchi <kawakazu80@gmail.com>
2019-10-29 01:09:20 +0900

    :star: new: add commit customizing

--- ship.config.js
+++ ship.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  conventionalChangelogArgs: '-i CHANGELOG.md -s -n ./node_modules/git-commit-message-convention/convention.js',
+  formatCommitMessage: ({ version, mergeStrategy, baseBranch }) =>
+    mergeStrategy.toSameBranch.includes(baseBranch)
+      ? `:package: release: v${version}`
+      : `:pencil: docs: prepare v${version}`,
+  formatPullRequestTitle: ({ version }) => `:package: release: v${version}`,
   publishCommand: () => 'echo dummy publish!',
   mergeStrategy: {
     toSameBranch: ['legacy'],
